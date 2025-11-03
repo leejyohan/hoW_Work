e7e0164ee331ad59fdf12cea2602c6e3e8695d48
leejyohan <chyun1529@gmail.com>
2025-11-03 22:20:02 +0900

    위젯 생성중

---
+++ widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  widget
+//
+//  Created by 이지현 on 11/3/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by 이지현 on 11/3/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
