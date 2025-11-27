6865ccd39c03c153785cb6b03c0e0a7a10008ad6
leejyohan <chyun1529@gmail.com>
2025-11-27 16:38:05 +0900

    급여계산,위젯 수정

---
+++ hoW_Work/Salary.swift
@@ -0,0 +1,18 @@
+//
+//  Salary.swift
+//  hoW_Work
+//
+//  Created by 이지현 on 11/27/25.
+//
+
+import SwiftUI
+
+struct Salary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Salary()
+}
---
+++ hoW_Work/Weekly.swift
@@ -0,0 +1,8 @@
+//
+//  Weekly.swift
+//  hoW_Work
+//
+//  Created by 이지현 on 11/6/25.
+//
+
+import Foundation
